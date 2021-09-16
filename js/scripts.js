ba8e0de9e336d85498788ed4f1c6b08ebaf74c93
Brian A <89669426+Barget20@users.noreply.github.com>
2021-09-16 17:23:53 -0400

    updating to include proper folders

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+
+alert('Hello World');
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+let favoriteFood = 'vegetarian tacos';
+document.write(favoriteFood);
+console.log(favoriteFood);
