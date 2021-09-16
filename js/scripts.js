e7f9627b1bc2e802236af3c914deff74a19eee1e
Brian A <89669426+Barget20@users.noreply.github.com>
2021-09-16 18:24:48 -0400

    Updated scripts to current Pokemon Index Project
    updated the files to be current after fixing the repositories today with Drew.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 
-alert('Hello World');
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
-let favoriteFood = 'vegetarian tacos';
-document.write(favoriteFood);
-console.log(favoriteFood);
+var pokemonList;
+
+let pokemons = [{name: "Bulbasaur", height: "2.04", type: "water"},{name: "Vulpix", height: "2.0", type: "fire"},{name: "Growlithe", height: "2.04", type: "fire"},
+{name: "Houndour", height: "2.0", type: ["dark", "fire"]}];
+
+console.log(pokemons); //I wish it would show the results like in the console section in browser// 
+
