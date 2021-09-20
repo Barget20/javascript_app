946cd070eb3fa944cb50147251e75b8d97874443
Brian A <89669426+Barget20@users.noreply.github.com>
2021-09-19 20:27:35 -0400

    Added for/if/else to pokemon list. changed height to size

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,32 @@
 
 var pokemonList;
 
-let pokemons = [{name: "Bulbasaur", height: "2.04", type: "water"},{name: "Vulpix", height: "2.0", type: "fire"},{name: "Growlithe", height: "2.04", type: "fire"},
-{name: "Houndour", height: "2.0", type: ["dark", "fire"]}];
+let pokemons = [
+    {name: "Bulbasaur", size: 2.04, type: "water"},
+    {name: "Vulpix", size: 2.0, type: "fire"},
+    {name: "Growlithe", size: 2.04, type: "fire"},
+    {name: "Houndour", size: 2.0, type: ["dark", "fire"]},
+    {name: "Oddish", size: 1.08, type: ["grass", "poison"]},
+    {name: "Golem", size: 4.07, type: ["rock", "ground"]}
+];
 
-console.log(pokemons); //I wish it would show the results like in the console section in browser// 
+console.log(pokemons);
 
+
+for (let i = 0; i < pokemons.size; i++){
+   if (pokemons[i].size > 3) {
+    console.log("this is a big pokemon!!");
+}else if (pokemons[i].size > 1.5 && pokemons[i].size < 3) {
+    console.log("this is an average pokemon");
+}else {
+    console.log("this is a small pokemon");
+}
+
+//let pokemons = ["Bulbasaur", "Vulpix", "Growlithe", "Houndour", "Oddish", "Golem",];
+//let text = "";
+//let i = 0;
+//for (;pokemons[i];){
+  //  text = text + " " + pokemons[i];
+    //i++;
+//}
+//console.log(text); 
