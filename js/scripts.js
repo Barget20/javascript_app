984e77b1f8350fd6d505905ea57817fad9ef561c
Brian A <89669426+Barget20@users.noreply.github.com>
2021-09-30 19:49:08 -0400

    pokemonRepository added
    My first stab at adding the  pokemonRepository with IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,44 @@
-var pokemonList;
-
-let pokemons = [
-    {name: "Bulbasaur", size: 2.04, type: "water"},
-    {name: "Vulpix", size: 2.0, type: "fire"},
-    {name: "Growlithe", size: 2.04, type: "fire"},
-    {name: "Houndour", size: 2.0, type: ["dark", "fire"]},
-    {name: "Oddish", size: 1.08, type: ["grass", "poison"]},
-    {name: "Golem", size: 4.07, type: ["rock", "ground"]}
-];
-
-console.log(pokemons);
-
-pokemons.forEach(function (pokemons){
-  document.write
-  if (pokemons.size > 3) {
-    document.write("<p> name:" + pokemons.name + " height: " + pokemons.size + " wow this is a big pokemon!</p>");
-}else if (pokemons.size > 1.5 && pokemons.size < 3) {
-    document.write("<p> name:" + pokemons.name + " height: " + pokemons.size + " this is an average pokemon</p>");
-}else {
-    document.write("<p> name:" + pokemons.name + " height: " + pokemons.size + " this is a small pokemon</p>");
-}
-});
+var pokemonRepository = (function (){
+    var pokemonList;
+    
+    let pokemons = [
+        {name: "Bulbasaur", size: 2.04, type: "water"},
+        {name: "Vulpix", size: 2.0, type: "fire"},
+        {name: "Growlithe", size: 2.04, type: "fire"},
+        {name: "Houndour", size: 2.0, type: ["dark", "fire"]},
+        {name: "Oddish", size: 1.08, type: ["grass", "poison"]},
+        {name: "Golem", size: 4.07, type: ["rock", "ground"]}
+    ];
+    })();
+    
+      function add(pokemon) {
+          pokemonList.push(pokemon);
+        }
+    
+      function getAll() {
+          return pokemonList;
+        }
+      
+      return {
+        add: add
+        getAll: getAll
+      };
+    })();
+    
+    console.log ( pokemonRepository.getAll());
+    console.log ( pokemonRepository.add());
+    
+    
+    pokemons.forEach(function (pokemons){
+      document.write
+      if (pokemons.size > 3) {
+        document.write("<p> name:" + pokemons.name + " height: " + pokemons.size + " wow this is a big pokemon!</p>");
+    }else if (pokemons.size > 1.5 && pokemons.size < 3) {
+        document.write("<p> name:" + pokemons.name + " height: " + pokemons.size + " this is an average pokemon</p>");
+    }else {
+        document.write("<p> name:" + pokemons.name + " height: " + pokemons.size + " this is a small pokemon</p>");
+    }
+    });
 
 //let pokemons = ["Bulbasaur", "Vulpix", "Growlithe", "Houndour", "Oddish", "Golem",];
 //let text = "";
